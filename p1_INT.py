2d4db64be885e8ba8e5c3030ca4edc9609c1773f
avonhavensyr <anvonhav@syr.edu>
2026-09-24 17:22:24 -0400

    Adding new test case from Griffiths textbook. Wrapping up for the day.

--- p1_INT.py
+++ p1_INT.py
@@ -52,6 +52,13 @@ def psi_isw(x, a, n, **kwargs):
     psi = np.sqrt(2 / a) * np.sin((n * np.pi * x) / a)
     return psi
 
+def dpsi_isw(x, a, n, **kwargs):
+    dpsi = np.sqrt(2 / a) * ((n * np.pi) / a) * np.cos((n * np.pi * x) / a)
+    return dpsi
+
+def d2psi_is2(x, a, n, **kwargs):
+    d2psi = np.sqrt(2 / a) * (((n * np.pi) / a)**2) * -1 * np.sin((n * np.pi * x) / a)
+
 def E_isw(n, a, m = 1, **kwargs):
     return (((n * np.pi * hbar)/a) ** 2) * (1 / (2 * m))
 
@@ -115,3 +122,43 @@ step = 0.1
 test_int = fourierTr(0, a, step, psi_isw, n = 1, a = a)
 test_int = expecVal2(psi_isw, 0, a, step, x_func = squared, n = 1, a = a)
 print(test_int)
+
+
+# I'm stuck so I started flipping through Griffiths chapter 2
+# until I found something semi-interesting I could do
+
+# Griffiths 2.45
+# Nodes at n = 3: a/3, 2a/3
+x1 = a/3
+x2 = (2 * a)/3
+
+dpsi_32 = dpsi_isw(x = x2, a = a, n = 3)
+dpsi_31 = dpsi_isw(x = x1, a = a, n = 3)
+
+psi_21 = psi_isw(x = x1, a = a, n = 2)
+psi_22 = psi_isw(x = x2, a = a, n = 2)
+m = 1
+E3 = E_isw(3, a)
+E2 = E_isw(2, a)
+
+def psitpsi(xmin, n1, n2, psi, a, **kwargs):
+    psi1 = psi_isw(xmin, a, n1)
+    psi2 = psi_isw(xmin, a, n2)
+    psi_prod = psi1 * psi2
+    return psi_prod
+
+int_psiprod = Reimann(psitpsi, x1, x2, step, n1 = 3, n2 = 2, a = a, psi = psi_isw)
+lhs = (dpsi_32 * psi_22) - (dpsi_31 * psi_21)
+rhs = ((2*m)/hbar) * (E2 - E3) * int_psiprod
+
+
+
+#print(E2)
+#print(E3)
+print(f'Left: {lhs}')
+print(f'Right: {rhs}')
+
+# To Do:
+#   Improve comments– got kinda lazy there
+#   Look into Griffiths problem further– do more psi states probably
+#   Try part a with the Euler/RK4 calculators and see how the results match up with the integrator
